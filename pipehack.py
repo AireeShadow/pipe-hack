a07e45750c7e58ff436d2bb9006dd842d7a3b2df
Anton Chernykh <30685027+AireeShadow@users.noreply.github.com>
2020-05-10 21:12:22 +0700

    Refactoring, additional methods

--- pipehack.py
+++ pipehack.py
@@ -25,27 +25,27 @@ class PipeWindow(QWidget):
             elif 'east' in directions:
                 pic = 'pic/half_east.png'
         elif button_type == 'corner':
-            if ('north' and 'east') in directions:
+            if all(x in directions for x in ['north', 'east']):
                 pic = 'pic/corner_north_east.png'
-            elif ('north' and 'west') in directions:
-                pic = 'pic/corner_north_east.png'
-            elif ('south' and 'west') in directions:
+            elif all(x in directions for x in ['north', 'west']):
+                pic = 'pic/corner_north_west.png'
+            elif all(x in directions for x in ['south', 'east']):
                 pic = 'pic/corner_south_east.png'
-            elif ('south' and 'east') in directions:
-                pic = 'pic/corner_south_east.png'
+            elif all(x in directions for x in ['south', 'west']):
+                pic = 'pic/corner_south_west.png'
         elif button_type == 'straight':
-            if ('north' and 'south') in directions:
+            if all(x in directions for x in ['south', 'north']):
                 pic = 'pic/straight_north_south.png'
-            elif ('east' and 'west') in directions:
+            elif all(x in directions for x in ['east', 'west']):
                 pic = 'pic/straight_west_east.png' 
         elif button_type == 'three':
-            if ('north' and 'south' and 'east') in directions:
+            if all(x in directions for x in ['south', 'east', 'north']):
                 pic = 'pic/three_north_south_east.png'
-            elif ('north' and 'south' and 'west') in directions:
+            elif all(x in directions for x in ['south', 'west', 'north']):
                 pic = 'pic/three_north_south_west.png'
-            elif ('north' and 'east' and 'west') in directions:
+            elif all(x in directions for x in ['west', 'east', 'north']):
                 pic = 'pic/three_north_west_east.png'    
-            elif ('south' and 'east' and 'west') in directions:
+            elif all(x in directions for x in ['south', 'east', 'west']):
                 pic = 'pic/three_south_west_east.png' 
         elif button_type == 'intersection':
             pic = 'pic/intersection.png'
@@ -61,6 +61,7 @@ class PipeWindow(QWidget):
                         return False
         else:
             return True
+        
     def get_neighbours_list(self, row: int, col: int) -> list:
         length = len(self.button_layout_list) - 1
         up_row = row - 1
@@ -78,9 +79,105 @@ class PipeWindow(QWidget):
             final_list.append((row, right_col))
         return final_list
     
+    def set_connections(self) -> None:
+        for row_index, row_list in enumerate(self.button_layout_list):
+            for col_index, col_dict in enumerate(row_list):
+                neighbours_list = self.get_neighbours_list(row=row_index, col=col_index)
+                current_button_directions_list = []
+                for direction_list in col_dict['locations']:
+                    for direction_dict in direction_list:
+                        current_button_directions_list.append(direction_dict['direction'])
+                for neighbour_coords_tuple in neighbours_list:
+                    neighbour_button_directions_list = []
+                    neighbour_row = neighbour_coords_tuple[0]
+                    neighbour_col = neighbour_coords_tuple[1]
+                    for neighbour_directions_dict in self.button_layout_list[neighbour_row][neighbour_col]['locations']:
+                        neighbour_button_directions_list.append(neighbour_directions_dict['direction'])
+                    if self.button_layout_list[neighbour_row][neighbour_col]['type'] != 'empty':
+                        if neighbour_row < row_index:
+                            #up
+                            if 'north' in current_button_directions_list and 'south' in neighbour_button_directions_list:
+                                for direction_index, direction_dict in enumerate(self.button_layout_list[row_index][col_index]['locations']):
+                                    if direction_dict['direction'] == 'north':
+                                        self.button_layout_list[row_index][col_index]['locations'][direction_index]['connected'] = True
+                            else:
+                                for direction_index, direction_dict in enumerate(self.button_layout_list[row_index][col_index]['locations']):
+                                    if direction_dict['direction'] == 'north':
+                                        self.button_layout_list[row_index][col_index]['locations'][direction_index]['connected'] = False
+                        elif neighbour_row > row_index:
+                            #down
+                            if 'south' in current_button_directions_list and 'north' in neighbour_button_directions_list:
+                                for direction_index, direction_dict in enumerate(self.button_layout_list[row_index][col_index]['locations']):
+                                    if direction_dict['direction'] == 'south':
+                                        self.button_layout_list[row_index][col_index]['locations'][direction_index]['connected'] = True
+                            else:
+                                for direction_index, direction_dict in enumerate(self.button_layout_list[row_index][col_index]['locations']):
+                                    if direction_dict['direction'] == 'south':
+                                        self.button_layout_list[row_index][col_index]['locations'][direction_index]['connected'] = False
+                        elif neighbour_col < col_index:
+                            #left
+                            if 'west' in current_button_directions_list and 'east' in neighbour_button_directions_list:
+                                for direction_index, direction_dict in enumerate(self.button_layout_list[row_index][col_index]['locations']):
+                                    if direction_dict['direction'] == 'west':
+                                        self.button_layout_list[row_index][col_index]['locations'][direction_index]['connected'] = True
+                            else:
+                                for direction_index, direction_dict in enumerate(self.button_layout_list[row_index][col_index]['locations']):
+                                    if direction_dict['direction'] == 'west':
+                                        self.button_layout_list[row_index][col_index]['locations'][direction_index]['connected'] = False
+                        elif neighbour_position > col_index:
+                            #right
+                            if 'east' in current_button_directions_list and 'west' in neighbour_button_directions_list:
+                                for direction_index, direction_dict in enumerate(self.button_layout_list[row_index][col_index]['locations']):
+                                    if direction_dict['direction'] == 'east':
+                                        self.button_layout_list[row_index][col_index]['locations'][direction_index]['connected'] = True
+                            else:
+                                for direction_index, direction_dict in enumerate(self.button_layout_list[row_index][col_index]['locations']):
+                                    if direction_dict['direction'] == 'east':
+                                        self.button_layout_list[row_index][col_index]['locations'][direction_index]['connected'] = False
+    
     def rotate_button(self, row: int, col: int) -> None:
         button_dict = self.button_layout_list[row][col]
-        
+        if button_dict['type'] == 'half':
+            if button_dict['locations'][0]['direction'] == 'north':
+                self.button_layout_list[row][col]['locations'][0]['direction'] = 'east'
+                pic = self.image_picker(button_type='half', directions=['east'])
+            elif button_dict['locations'][0]['direction'] == 'east':
+                self.button_layout_list[row][col]['locations'][0]['direction'] = 'south'
+                pic = self.image_picker(button_type='half', directions=['south'])
+            elif button_dict['locations'][0]['direction'] == 'south':
+                self.button_layout_list[row][col]['locations'][0]['direction'] = 'west'
+                pic = self.image_picker(button_type='half', directions=['west'])
+            elif button_dict['locations'][0]['direction'] == 'west':
+                self.button_layout_list[row][col]['locations'][0]['direction'] = 'north'
+                pic = self.image_picker(button_type='half', directions=['north'])
+        elif button_dict['type'] == 'corner':
+            if button_dict['locations'][0]['direction'] == 'north' and button_dict['locations'][1]['direction'] == 'east':
+                self.button_layout_list[row][col]['locations'][0]['direction'] = 'east'
+                self.button_layout_list[row][col]['locations'][1]['direction'] = 'south'
+                pic = self.image_picker(button_type='corner', directions=['east', 'south'])
+            elif button_dict['locations'][0]['direction'] == 'east' and button_dict['locations'][1]['direction'] == 'south':
+                self.button_layout_list[row][col]['locations'][0]['direction'] = 'south'
+                self.button_layout_list[row][col]['locations'][1]['direction'] = 'west'
+                pic = self.image_picker(button_type='corner', directions=['west', 'south'])
+            elif button_dict['locations'][0]['direction'] == 'south' and button_dict['locations'][1]['direction'] == 'west':
+                self.button_layout_list[row][col]['locations'][0]['direction'] = 'west'
+                self.button_layout_list[row][col]['locations'][1]['direction'] = 'north'
+                pic = self.image_picker(button_type='corner', directions=['west', 'north'])
+            elif button_dict['locations'][0]['direction'] == 'west' and button_dict['locations'][1]['direction'] == 'north':
+                self.button_layout_list[row][col]['locations'][0]['direction'] = 'north'
+                self.button_layout_list[row][col]['locations'][1]['direction'] = 'east'
+                pic = self.image_picker(button_type='corner', directions=['north', 'east'])
+        elif button_dict['type'] == 'straight':
+            if button_dict['locations'][0]['direction'] == 'north' and button_dict['locations'][1]['direction'] == 'south':
+                self.button_layout_list[row][col]['locations'][0]['direction'] = 'west'
+                self.button_layout_list[row][col]['locations'][1]['direction'] = 'east'
+                pic = self.image_picker(button_type='straight', directions=['east', 'west'])
+            elif button_dict['locations'][0]['direction'] == 'east' and button_dict['locations'][1]['direction'] == 'south':
+                self.button_layout_list[row][col]['locations'][0]['direction'] = 'south'
+                self.button_layout_list[row][col]['locations'][1]['direction'] = 'west'
+                pic = self.image_picker(button_type='corner', directions=['west', 'south'])
+        self.button_layout_list[row][col]['image'] = pic
+                
     def initUI(self):
         grid = QGridLayout()
         self.setLayout(grid)
